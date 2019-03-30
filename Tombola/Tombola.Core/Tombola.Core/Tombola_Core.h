5c80ccce88bf525c52ea5f6da036d5dbf5590836
Brian Williams <emial.brianwilliams@gmail.com>
2019-03-30 13:46:07 +0000

    Add core project

---
+++ Tombola/Tombola.Core/Tombola.Core/Tombola_Core.h
@@ -0,0 +1,19 @@
+//
+//  Tombola_Core.h
+//  Tombola.Core
+//
+//  Created by Brian on 30/03/2019.
+//  Copyright © 2019 Eviathan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Tombola_Core.
+FOUNDATION_EXPORT double Tombola_CoreVersionNumber;
+
+//! Project version string for Tombola_Core.
+FOUNDATION_EXPORT const unsigned char Tombola_CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tombola_Core/PublicHeader.h>
+
+
